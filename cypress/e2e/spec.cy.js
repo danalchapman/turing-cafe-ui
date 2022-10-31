7ad8a570e3a2f596f5c3a5b74789170248841795
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-10-31 13:10:33 -0500

    Add tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,30 @@
 describe('Dashboard flows', () => {
 
   beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/reservations"), [
+      {
+        "id": 1,
+        "name": "Christie",
+        "date": "12/29",
+        "time": "7:00",
+        "number": 12
+      },
+      {
+        "id": 2,
+        "name": "Leta",
+        "date": "4/5",
+        "time": "7:00",
+        "number": 2
+      },
+      {
+        "id": 3,
+        "name": "Pam",
+        "date": "1/21",
+        "time": "6:00",
+        "number": 4
+      }
+    ]
+
     cy.visit('http://localhost:3000')
   })
 
@@ -11,38 +35,12 @@ describe('Dashboard flows', () => {
       .should('include', 'Name')
   })
 
-  it('Should be able to select the name, date, time, and number inputs and fill them with the corresponding values', () => {
-    cy.get('input[name="name"]') .type('Dana') .should('have.value', 'Dana')
-      .get('input[name="date"]') .type('10/31') .should('have.value', '10/31')
-      .get('input[name="time"]') .type('6:00') .should('have.value', '6:00')
-      .get('input[name="number"]') .type('2') .should('have.value', '2')
+  it('Should be able to select and fill the form inputs and be able to submit the new reservation', () => {
+    cy.get('input[name="name"]').type('Dana').should('have.value', 'Dana')
+      .get('input[name="date"]').type('10/31').should('have.value', '10/31')
+      .get('input[name="time"]').type('6:00').should('have.value', '6:00')
+      .get('input[name="number"]').type('2').should('have.value', '2')
+      .get('.make-res-btn').click()
+      .get('.res-box').get('h4').contains('Dana')
   })
-
-  it('Should be able to see pre-existing reservations', () => {
-    cy.intercept("GET", "http://localhost:3001/api/v1/reservations/1", {
-      id: 1,
-      name: "Christie",
-      date: "12/29",
-      time: "7:00",
-      number: 12
-    })
-
-    cy.intercept("GET", "http://localhost:3001/api/v1/reservations/2", {
-      id: 1,
-      name: "Leta",
-      date: "4/5",
-      time: "7:00",
-      number: 2
-    })
-
-    cy.intercept("GET", "http://localhost:3001/api/v1/reservations/3", {
-      id: 1,
-      name: "Pam",
-      date: "1/21",
-      time: "6:00",
-      number: 4
-    })
-  })
-
-  // test to check user flow of adding a new res to the page
 })
